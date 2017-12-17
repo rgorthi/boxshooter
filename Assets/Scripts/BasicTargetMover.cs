335ef14b34123afd80adb63d46a61f8d49ebeb06
ravisatviK <rockingravi.satvik@gmail.com>
2017-12-17 18:07:11 +0530

    First commit

---
+++ Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour {
+	public bool doSpin = true;
+	public float spinSpeed = 180.0f;
+	public bool doMove = false;
+	public float motionMagnitude = 0.1f;
+	// Update is called once per frame
+	void Update () {
+		if (doSpin) {
+			gameObject.transform.Rotate (Vector3.up * spinSpeed * Time.deltaTime);
+		}
+		if (doMove) {
+			gameObject.transform.Translate (Vector3.up * Mathf.Cos (Time.timeSinceLevelLoad) * motionMagnitude);
+		}
+	}
+}
